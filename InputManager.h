cd5049ebeb42d9a3ab50ca4c91d012a48a1e2d70
P1Akiraaa <reocreuxalexis@gmail.com>
2025-01-31 10:59:48 +0100

    new InputManager

--- InputManager.h
+++ InputManager.h
@@ -2,52 +2,77 @@
 #include "CoreMinimal.h"
 #include "Singleton.h"
 
-using KeyPressed = Event::KeyPressed;
-using Code = Keyboard::Key;
-using Click = Mouse::Button;
-using Close = Event::Closed;
-
-struct InputData
+namespace Input
 {
-	vector<Code> codes;
-	bool isAnyKey;
-	function<void()> callback;
+	using KeyPressed = Event::KeyPressed;
+	using Code = Keyboard::Key;
+	using Clicked = Mouse::Button;
+	using Click = Event::MouseButtonPressed;
+	using Close = Event::Closed;
 
-	InputData(const function<void()> _callback, const vector<Code> _codes, const bool _isAnyKey = false)
+	struct InputData
 	{
-		callback = _callback;
-		codes = _codes;
-		isAnyKey = _isAnyKey;
-	}
+		vector<Code> codes;
+		bool isAnyKey;
+		function<void()> callback;
 
-	FORCEINLINE bool TryToExecute(const KeyPressed* _key)
-	{
-		if (!isAnyKey && !ContainsKey(_key->code)) return false;
-
-		callback();
-		return true;
-	}
-
-	FORCEINLINE bool ContainsKey(const Code& _currentCode) const
-	{
-		for (const Code& _code : codes)
+		InputData(const function<void()> _callback, const vector<Code> _codes, const bool _isAnyKey = false)
 		{
-			if (_currentCode == _code) return true;
+			callback = _callback;
+			codes = _codes;
+			isAnyKey = _isAnyKey;
 		}
 
-		return false;
-	}
-};
+		FORCEINLINE bool TryToExecute(const KeyPressed* _key)
+		{
+			if (!isAnyKey && !ContainsKey(_key->code)) return false;
 
-class InputManager : public Singleton<InputManager>
-{
-	vector<InputData> inputsData;
-	
-public:
-	void ConsumeInput(RenderWindow& _window);
-	void BindAction(const function<void()>& _callback, const Code& _code);
-	void BindAction(const function<void()>& _callback, const vector<Code>& _codes = {});
+			callback();
+			return true;
+		}
 
-private:
-	void CloseWindow(RenderWindow& _window);
-};
+		FORCEINLINE bool ContainsKey(const Code& _currentCode) const
+		{
+			for (const Code& _code : codes)
+			{
+				if (_currentCode == _code) return true;
+			}
+
+			return false;
+		}
+	};
+
+	struct ClickData
+	{
+		Clicked click;
+		function<void()> callback;
+
+		ClickData(const function<void()> _callback, const Clicked* _mouse, const bool _isAnyButton = false)
+		{
+			callback = _callback;
+			click = *_mouse;
+		}
+
+		FORCEINLINE bool TryToExecute(const Click* _click)
+		{
+			if (!(click == _click->button)) return false;
+			callback();
+			return true;
+		}
+
+	};
+	class InputManager : public Singleton<InputManager>
+	{
+		vector<InputData> inputsData;
+		vector<ClickData> clickData;
+
+	public:
+		void ConsumeInput(RenderWindow& _window);
+		void BindAction(const function<void()>& _callback, const Code& _code);
+		void BindAction(const function<void()>& _callback, const  Clicked& _click);
+		void BindAction(const function<void()>& _callback, const vector<Code>& _codes = {});
+
+	private:
+		void CloseWindow(RenderWindow& _window);
+	};
+}
