2e34c59a895892e97e4027ccc35166d5270eefef
P1Akiraaa <reocreuxalexis@gmail.com>
2025-02-05 16:59:54 +0100

    on pleur nos morts sur les box

--- VampireArena.cpp
+++ VampireArena.cpp
@@ -47,7 +47,9 @@ void VampireArena::InitButton()
     //TODO new menu
 	HorizontalBox* _box = M_HUD.CreateWidget<HorizontalBox>(BoxData(0, Vector2f(600.0f, 400.0f)));
 	Label* _label = M_HUD.CreateWidget<Label>("test", World, "Bloody_Gothic_Empire", TTF);
-	_label->SetZOrder(1);
+	_box->SetZOrder(1);
+	M_HUD.AddToViewport(_box);
+	_label->SetZOrder(2);
 	_box->AddWidget(_label);
 	_box->SetPosition(Vector2f(500.0f, 500.0f));
     /*MeshActor* _mainBox = Level::SpawnActor(MeshActor(RectangleShapeData(Vector2f(500.0f, 800.0f), "Character_Sheet")));
